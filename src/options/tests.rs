63aab8c646c736942d5a8598bad9cfb82d77e099
John Doe <rangolit@gmail.com>
2025-11-04 20:30:42 +0100

    Split test and code for options

---
+++ src/options/tests.rs
@@ -0,0 +1,45 @@
+use super::*;
+
+#[test]
+fn find_block_size() {
+    let mut options = HashMap::new();
+    options.insert("blksize".to_string(), "1468".to_string());
+    let blk_size = Blksize::find_in(&options).unwrap();
+    assert_eq!(blk_size.block_size, 1468);
+    assert_eq!(
+        blk_size.as_key_pair(),
+        (BLKSIZE.to_string(), "1468".to_string())
+    );
+}
+
+#[test]
+fn find_tsize() {
+    let mut options = HashMap::new();
+    options.insert("tsize".to_string(), "0".to_string());
+    assert!(TSize::is_requested(&options));
+}
+
+#[test]
+fn find_timeout() {
+    let mut options = HashMap::new();
+    let timeout_value: usize = 10;
+    options.insert("timeout".to_string(), timeout_value.to_string());
+    let timeout = AckTimeout::find_in(&options).unwrap();
+    assert_eq!(timeout.timeout, timeout_value);
+}
+
+#[test]
+fn test_timeout_cap() {
+    let mut options = HashMap::new();
+    options.insert("timeout".to_string(), (ACK_TIMEOUT_LIMIT + 1).to_string());
+    let find_result = AckTimeout::find_in(&options);
+    assert!(find_result.is_none());
+}
+
+#[test]
+fn test_block_size_cap() {
+    let mut options = HashMap::new();
+    options.insert("blksize".to_string(), (BLOCK_SIZE_LIMIT + 1).to_string());
+    let find_result = Blksize::find_in(&options);
+    assert!(find_result.is_none());
+}
