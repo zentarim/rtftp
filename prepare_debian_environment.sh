3023d9025a5ec73f90d87ac46538805be70b3f87
John Doe <rangolit@gmail.com>
2026-02-28 20:47:23 +0100

    Fix prepare_debian_environment.sh
    Postinstall script name is not correct, apt-get is more
    suitable for scripts usage. The postinstall script
    name should be set only once in a variable.

--- prepare_debian_environment.sh
+++ prepare_debian_environment.sh
@@ -2,14 +2,15 @@
 set -eux
 cd "${0%/*}"
 
-apt install -y gcc libguestfs-dev guestfs-tools nbd-client
+apt-get install -y gcc libguestfs-dev guestfs-tools nbd-client
 
 source /etc/os-release
 
 if [ "${ID}" == "ubuntu" ]; then
   echo "Ubuntu OS found. Add kernel hook to allow 'sudo' group to read kernel image.
 See: https://bugs.launchpad.net/ubuntu/+source/linux/+bug/759725"
-  cat <<'EOF' >/etc/kernel/postinst.d/zz_kernel_libuestf_perm
+  POSTINSTALL_SCRIPT="/etc/kernel/postinst.d/zz_kernel_libguestf_perm"
+  cat <<'EOF' >${POSTINSTALL_SCRIPT}
 #!/bin/sh
 
 IMAGE_PATH="${2}"
@@ -22,8 +23,8 @@ else
   echo "WARNING: ${IMAGE_PATH} not found, consider manual preparation of the current kernel or running RTFTP with root privileges" >&2
 fi
 EOF
-chmod u+x /etc/kernel/postinst.d/zz_kernel_libuestf_perm
+chmod u+x "${POSTINSTALL_SCRIPT}"
 KERNEL_IMAGE_PATH="$(grep -oP 'BOOT_IMAGE=\K\S+' /proc/cmdline)"
 test -f "${KERNEL_IMAGE_PATH}" || KERNEL_IMAGE_PATH="/boot${KERNEL_IMAGE_PATH}"
-/etc/kernel/postinst.d/zz_kernel_libuestf_perm "" "${KERNEL_IMAGE_PATH}"
+${POSTINSTALL_SCRIPT} "" "${KERNEL_IMAGE_PATH}"
 fi
